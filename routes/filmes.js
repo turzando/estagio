56d686645d5cd10e06567fd3fd2762017cde6c25
Artur Silva <artur_antoniopw01@hotmail.com>
2021-05-06 11:24:49 -0300

    atualizando os métodos Criar e Listar conectado ao banco

--- routes/filmes.js
+++ routes/filmes.js
@@ -1,18 +1,55 @@
 const express = require('express')
 const router = express.Router()
+const mysql = require('../mysql').pool
 
 // Listar
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o GET dentro da rota de filmes'
+    // res.status(200).send({
+    //     mensagem: 'Usando o GET dentro da rota de filmes'
+    // })
+
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            'SELECT * FROM filmes;',
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error}) }
+                return res.status(200).send({ responde: resultado })
+            }
+        )
     })
+
+
 })
 
 // Criar
 router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Usando o POST dentro da rota de filmes'
+
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+
+        conn.query(
+            'INSERT INTO filmes (nome, descricao) values (?, ?);',
+            [req.body.nome, req.body.descricao],
+            (error, resultado, field) => {
+                conn.release()
+            
+                if (error) {
+                    return res.status(500).send({
+                        mensagem: error,
+                        response: null
+                    })
+                }
+
+                res.status(201).send({
+                    mensagem: 'Filme inserido com sucesso.',
+                    id_filme: resultado.insertId
+                })
+            }
+        )
     })
+
+    
 })
 
 // Atualizar
